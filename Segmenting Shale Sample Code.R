72bb66b26b8cf59496ab89accd30df6c3a6e9a86
Desion Hubbard <desionhubbard@gmail.com>
2018-05-24 19:37:51 +0000

    Update Segmenting Shale Sample Code.R

--- Segmenting Shale Sample Code.R
+++ Segmenting Shale Sample Code.R
@@ -24,19 +24,40 @@ print(mypic[[1]])display(mypic[[1]])summary(mypic[[1]])hist(mypic[[1]])str(mypic
 #Resize Image To Have Same Dimensions
 for(i in 1:22 ){mypic[[i]] <- resize(mypic[[i]],28,28)}str(mypic[1:4])
 
-#Convert Images Into Matrices By Reshaping(Take 28x28x3 Into Single Dimension 2,352)for (i in 1:22) {mypic[[i]] <- array_reshape(mypic[[i]],c(28,28,3)) }str(mypic)  #take note to the difference of structure after Reshaping
+#Convert Images Into Matrices By Reshaping(Take 28x28x3 Into Single Dimension 2,352)
+for (i in 1:22) {mypic[[i]] <- array_reshape(mypic[[i]],c(28,28,3)) }str(mypic)  #take note to the difference of structure after Reshaping
 
-#Combine Data Into One Using Rowbindtrainx <- NULLfor(i in 1:6){trainx <- rbind(trainx, mypic[[i]])}str(trainx)
-testx<-NULLtestx <- rbind(mypic[[7]],mypic[[8]],mypic[[9]],mypic[[19]],mypic[[20]],mypic[[21]],mypic[[22]])str(testx)#We'll Represent Train By: Not Shale=0, Shale=1    trainy <-c(0,0,0,0,0,0,1,1,1,1,1,1,1,1,1)testy <- c(0,0,0,1,1,1,1)
+#Combine Data Into One Using Rowbind
+trainx <- NULLfor(i in 1:6){trainx <- rbind(trainx, mypic[[i]])}str(trainx)
+testx<-NULLtestx <- rbind(mypic[[7]],mypic[[8]],mypic[[9]],mypic[[19]],mypic[[20]],mypic[[21]],mypic[[22]])str(testx)
 
-#One Hot Encoding Process Which Converts Categorical Varibles Into A Form That# Could Be Provided to ML Algorithms To do a Better Job In PredictiontrainLabels <- to_categorical(trainy)testLabels <- to_categorical(testy)
+#Represented Image Classification By: Not Shale=0, Shale=1    
+trainy <-c(0,0,0,0,0,0,1,1,1,1,1,1,1,1,1)testy <- c(0,0,0,1,1,1,1)
 
-#Creating The Modelmodel <- keras_model_sequential()model %>%  #Fully Connected Nueral Networks; 'relu' is most famous activation function  layer_dense(units = 256, activation = 'relu', input_shape =c(2352) ) %>%  layer_dense(units = 128, activation = 'relu')%>%  layer_dense(units = 2, activation = 'softmax')summary(model)
+#One Hot Encoding Process Which Converts Categorical Varibles Into A Form That Could Be Provided to ML Algorithms To do a Better Job In Prediction
+trainLabels <- to_categorical(trainy)testLabels <- to_categorical(testy)
 
-#Compile the Modelmodel %>%    compile(loss = 'binary_crossentropy',            optimizer =optimizer_rmsprop(),            metrics = c('accuracy'))
+#Creating The Modelmodel <- keras_model_sequential()model %>%  #Fully Connected Nueral Networks; 'relu' is most famous activation function  
+layer_dense(units = 256, activation = 'relu', input_shape =c(2352) ) %>%  
+    layer_dense(units = 128, activation = 'relu') %>%  
+    layer_dense(units = 2, activation = 'softmax')
+summary(model)
 
-# Fit Modelhistory <- model %>%        fit(trainx,            trainLabels,            epochs = 30,            batch_size= 32,            validation_split = 0.2) # 20% of data used for validation        plot(history)        # Model Evaluation & Prediction -train data        model %>% evaluate(trainx, trainLabels)                #accuracy will be shown in the form of $acc after this step        pred<- model %>% predict_classes(trainx)
+#Compile the Modelmodel %>%    
+    compile(loss = 'binary_crossentropy', optimizer =optimizer_rmsprop(),metrics = c('accuracy'))
+
+# Fit Modelhistory <- model %>%
+    fit(trainx, trainLabels, epochs = 30, batch_size= 32, validation_split = 0.2) # 20% of data used for validation        
+plot(history)        
+# Model Evaluation & Prediction -train data        
+model %>% 
+    evaluate(trainx, trainLabels)                #accuracy will be shown in the form of $acc after this step        
+pred<- model %>% 
+    predict_classes(trainx)
 
 #Create Confusion Matrixtable(Predicted=pred, Actual= trainy)
-prob <- model %>% predict_proba(trainx)
-#Column Bind The Probality Resultscbind(prob, Predicted = pred, Actual = trainy)
+prob <- model %>% 
+    predict_proba(trainx)
+
+#Column Bind The Probality Results
+cbind(prob, Predicted = pred, Actual = trainy)
